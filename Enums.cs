943fe37779c52797935e23c22615370f0237023b
SebastianMontes99 <62891233+SebastianMontes99@users.noreply.github.com>
2020-03-30 16:35:06 -0500

    Add files via upload

---
+++ Enums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAppUpc
+{
+    public static class Enums
+    {
+        public enum Status
+        {
+            Pending,
+            Disabled,
+            Approved
+        }
+        public enum OrderStatus
+        {
+            Pendign,
+            Approved,
+            Canceled,
+            Rejected
+        }
+    }
+}
---
+++ ValidationHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAppUpc
+{
+    public class ValidationHelper
+    {
+        public static bool ProductPendingOrDisabled(Enums.Status status)
+        {
+            var statuslist = new List< Enums.Status > ();
+            //validar el estado en que se encuentra el valor //
+            statuslist.Add(Enums.Status.Disabled);
+            statuslist.Add(Enums.Status.Pending);
+
+            return statuslist.Contains(status);
+        }
+    }
+}
